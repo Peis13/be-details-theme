839b7db7563cd0d4655c1528429c515a6aa7fdbf
Nicoló Paganelli <nicolo@comodolab.it>
2021-07-15 14:10:43 +0200

    allargata sezione intro

--- templates/home/intro.php
+++ templates/home/intro.php
@@ -1,7 +1,7 @@
 <div id="intro" class="intro sezione text-center">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 offset-lg-3">
+            <div class="col-lg-8 offset-lg-3">
                 <h2>I nostri occhiali</h2>
                 <div class="content"><?= get_field('home_intro') ?></div>
             </div>
