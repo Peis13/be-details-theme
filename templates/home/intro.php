cc5873c89df42ea0ec3f9392d74c261287751d9c
Nicoló Paganelli <nicolo@comodolab.it>
2021-10-20 09:38:44 +0200

    titolo intro home

--- templates/home/intro.php
+++ templates/home/intro.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 offset-lg-3">
-                <h2>I nostri occhiali</h2>
+                <h2><?= get_field('title_intro') ?></h2>
                 <div class="content"><?= get_field('home_intro') ?></div>
             </div>
         </div>
